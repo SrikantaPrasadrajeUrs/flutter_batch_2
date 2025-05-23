d78e6739b2d78e4af470eccc19a1fe76ac853c5f
SrikantaPrasadrajeUrs <srikantaprasadrajeurs456@gmail.com>
2025-05-23 07:59:28 +0530

    initial commit

---
+++ example1.dart
@@ -0,0 +1,28 @@
+// Object Oriented Programming (OOP) in Dart
+// Client side 
+// Single threaded - Isolate, compute - main 
+// Object
+import 'dart:math';
+
+void main(List<String> args) {
+  int randomNumber = Random().nextInt(3);
+  // control flow statement
+ for(int i = 0; i < 10000; i++) {
+    if(i==1){
+      return;
+    }
+    // not execute
+  }
+
+  // switch case
+  switch (randomNumber) {
+    case 0:
+      print("Heads");
+      break;
+    case 1:
+      print("Tails");
+      break;
+    default:
+      print("Edge");
+  }
+}
